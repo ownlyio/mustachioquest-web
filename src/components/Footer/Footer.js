29885aeb0657898dad6d756ecb59f229bf18fa42
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-02-21 08:34:46 +0800

    Change JS links to anchor tags

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,29 +3,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Footer.css'
 
 export default function Footer() {
-    const redirectPage = url => {
-        window.open(url, "_blank")
-    }
-
     return (
         <section id="footer">
             <div className="container">
                 <div className="d-flex justify-content-center align-items-center">
-                    <div className="footer-item cursor-pointer" onClick={() => redirectPage("https://facebook.com/mustachioverse")}>
+                    <a className="footer-item cursor-pointer" href="https://facebook.com/mustachioverse" target="_blank" rel="noreferrer">
                         <FontAwesomeIcon color="white" icon={faFacebook} size="3x" />
-                    </div>
-                    <div className="footer-item cursor-pointer" onClick={() => redirectPage("https://instagram.com/mustachioverse")}>
+                    </a>
+                    <a className="footer-item cursor-pointer" href="https://instagram.com/mustachioverse" target="_blank" rel="noreferrer">
                         <FontAwesomeIcon color="white" icon={faInstagram} size="3x" />
-                    </div>
-                    <div className="footer-item cursor-pointer" onClick={() => redirectPage("http://discord.gg/rfypRx4cjh")}>
+                    </a>
+                    <a className="footer-item cursor-pointer" href="https://mustachioverse.com/discord" target="_blank" rel="noreferrer">
                         <FontAwesomeIcon color="white" icon={faDiscord} size="3x" />
-                    </div>
-                    <div className="footer-item cursor-pointer" onClick={() => redirectPage("https://twitter.com/mustachioverse")}>
+                    </a>
+                    <a className="footer-item cursor-pointer" href="https://twitter.com/mustachioverse" target="_blank" rel="noreferrer">
                         <FontAwesomeIcon color="white" icon={faTwitter} size="3x" />
-                    </div>
-                    <div className="footer-item cursor-pointer" onClick={() => redirectPage("https://www.tiktok.com/@mustachioverse")}>
+                    </a>
+                    <a className="footer-item cursor-pointer" href="https://www.tiktok.com/@mustachioverse" target="_blank" rel="noreferrer">
                         <FontAwesomeIcon color="white" icon={faTiktok} size="3x" />
-                    </div>
+                    </a>
                 </div>
             </div>
         </section>
