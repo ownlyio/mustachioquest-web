10e33396d704eee9a164dad6d27a6e03b4186403
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-02-28 13:33:42 +0800

    Add more sneak peeks

--- src/components/SneakPeek/SneakPeek.js
+++ src/components/SneakPeek/SneakPeek.js
@@ -1,9 +1,12 @@
 import './SneakPeek.css'
 
 // images
-import sneak1 from '../../images/sneakpeeks/sneakpeek-forge.png'
+import sneak6 from '../../images/sneakpeeks/3-mexico.png'
+import sneak5 from '../../images/sneakpeeks/2-desert-pit.png'
+import sneak4 from '../../images/sneakpeeks/1-menu-update.png'
+import sneak3 from '../../images/sneakpeeks/sneakpeek-forge.png'
 import sneak2 from '../../images/sneakpeeks/sneakpeek-blacksmith.jpg'
-import sneak3 from '../../images/sneakpeeks/sneakpeek-mainmenu.png'
+import sneak1 from '../../images/sneakpeeks/sneakpeek-mainmenu.png'
 
 import bordertop from '../../images/sneakpeek-top.png'
 import borderbot from '../../images/sneakpeek-bot.png'
@@ -11,8 +14,26 @@ import borderbot from '../../images/sneakpeek-bot.png'
 export default function SneakPeek() {
     const sneak = [
         {
-            id: 1,
-            thumbnail: sneak1,
+            id: 6,
+            thumbnail: sneak6,
+            link: "https://discord.com/channels/878131568806334464/935073739878563840/946207407271469076",
+            title: "Mexico on the Desert"
+        },
+        {
+            id: 5,
+            thumbnail: sneak5,
+            link: "https://discord.com/channels/878131568806334464/935073739878563840/945292032870580225",
+            title: "Sneak Peek on the Desert Pit"
+        },
+        {
+            id: 4,
+            thumbnail: sneak4,
+            link: "https://twitter.com/mustachioverse/status/1495748029005320198",
+            title: "Dev Update: MetaMask wallet integration, Loading Mustachio NFTs, and Start Page and Character Selection"
+        },
+        {
+            id: 3,
+            thumbnail: sneak3,
             link: "https://twitter.com/mustachioverse/status/1495748029005320198",
             title: "Enter the Forge!"
         },
@@ -23,8 +44,8 @@ export default function SneakPeek() {
             title: "Take your pick, you'll find no second-rate items with the Blacksmith"
         },
         {
-            id: 3,
-            thumbnail: sneak3,
+            id: 1,
+            thumbnail: sneak1,
             link: "https://www.youtube.com/watch?v=eLce8nuH0Yo",
             title: "Dev Update: Temporary Main Menu, Loading Bar and Transitions, and Loading to Different Scenes"
         },
