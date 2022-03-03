63a244121ec516f3e5b687b6734e7f891548149a
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-03-03 16:10:50 +0800

    Add columns for each type of Mustachio

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,5 +1,7 @@
 import './Table.css'
 
+import logo from '../../images/MQ_logo_white.png'
+
 import bordertl from '../../images/updates-tl.png'
 import bordertr from '../../images/updates-tr.png'
 import borderbl from '../../images/updates-bl.png'
@@ -9,9 +11,41 @@ export default function Table() {
     return (
         <section id="mq-table">
             <div className="container">
+                <p className="updates-title gotham-black text-white text-center font-size-170 mb-5">WHAT MUSTACHIOS CAN I OWN?</p>
                 <div className="row justify-content-center align-items-start">
-                    <p className="updates-title gotham-black text-white text-center font-size-170 mb-5">MEET THE MUSTACHIOS</p>
+                    <div className="col-12 col-md-4">
+                        <div className="mq-table-types">
+                            <div className="mq-table-types-img">
+                                <img className="w-100" src={logo} alt="Mustachio Royals" />
+                            </div>
 
+                            <p className="mq-table-types-title gotham-black font-size-150 text-white text-center">MUSTACHIO ROYALS</p>
+                            <p className="mq-table-types-sub gotham-semibold font-size-130 text-white text-center mb-2">1-100 MUSTACHIOS</p>
+                            <p className="mq-table-types-sub gotham-light font-size-120 text-white text-center">The First 100 Mustachios or the <b>"Genesis Set"</b> that can be minted at 0.4ETH!</p>
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-4">
+                        <div className="mq-table-types">
+                            <div className="mq-table-types-img">
+                                <img className="w-100" src={logo} alt="Mustachio Royals" />
+                            </div>
+
+                            <p className="mq-table-types-title gotham-black font-size-150 text-white text-center">MUSTACHIO RULERS</p>
+                            <p className="mq-table-types-sub gotham-semibold font-size-130 text-white text-center mb-2">101-300 MUSTACHIOS</p>
+                            <p className="mq-table-types-sub gotham-light font-size-120 text-white text-center">Stake 15 Million OWN at <a href="https://stake.ownly.market" className="link-color-2">https://stake.ownly.market</a> without unstaking for 30 Days to earn 1 Mustachio Ruler NFT.</p>
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-4">
+                        <div className="mq-table-types">
+                            <div className="mq-table-types-img">
+                                <img className="w-100" src={logo} alt="Mustachio Redeemers" />
+                            </div>
+
+                            <p className="mq-table-types-title gotham-black font-size-150 text-white text-center">MUSTACHIO REDEEMERS</p>
+                            <p className="mq-table-types-sub gotham-semibold font-size-130 text-white text-center mb-2">10,000 MUSTACHIOS</p>
+                            <p className="mq-table-types-sub gotham-light font-size-120 text-white text-center">10,000 Generative 3D Mustachios launching this Q2 2022</p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
