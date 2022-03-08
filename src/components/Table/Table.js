298c1681536bd94ccbd84be2af323e447c21a131
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-03-08 15:06:26 +0800

    Update content, change logos for each type and add CTAs

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,8 +1,8 @@
 import './Table.css'
 
-import royals from '../../images/MQ_royals.png'
-import rulers from '../../images/MQ_rulers.png'
-import redeemers from '../../images/MQ_redeemers.png'
+import pathfinders from '../../images/MQ_pathfinders.png'
+import marauders from '../../images/MQ_marauders.png'
+import rascals from '../../images/MQ_rascals.png'
 import greenCheck from '../../images/check-3d-green.png'
 import orangeCheck from '../../images/check-3d-orange.png'
 import goldCheck from '../../images/check-3d-gold.png'
@@ -23,27 +23,27 @@ export default function Table(props) {
                     <div className="col-12 col-md-4">
                         <div className="mq-table-types">
                             <div className="mq-table-types-img">
-                                <img className="w-100" src={royals} alt="Mustachio Royals" />
+                                <img className="w-100" src={pathfinders} alt="Mustachio Pathfinders" />
                             </div>
 
                             <p className="mq-table-types-sub gotham-semibold font-size-130 text-white text-center mb-2">MUSTACHIOS #1 to #100</p>
-                            <p className="mq-table-types-sub gotham-light font-size-120 text-white text-center">The First 100 Mustachios or the <b>"Genesis Set"</b>. <button onClick={props.mintBtn} className="btn link-color-2 p-0 font-size-100" target="_blank" rel="noreferrer">MINT NOW!</button></p>
+                            <p className="mq-table-types-sub gotham-light font-size-120 text-white text-center">The First 100 Mustachios or the <b>"Genesis Set"</b>. <button onClick={props.mintBtn} className="btn link-color-2 p-0 font-size-100">MINT NOW!</button></p>
                         </div>
                     </div>
                     <div className="col-12 col-md-4">
                         <div className="mq-table-types">
                             <div className="mq-table-types-img">
-                                <img className="w-100" src={rulers} alt="Mustachio Royals" />
+                                <img className="w-100" src={marauders} alt="Mustachio Marauders" />
                             </div>
 
                             <p className="mq-table-types-sub gotham-semibold font-size-130 text-white text-center mb-2">MUSTACHIOS #101 to #300</p>
-                            <p className="mq-table-types-sub gotham-light font-size-120 text-white text-center">Stake 15M OWN for 60 Days at <a href="https://ownly.io/stake" className="link-color-2" target="_blank" rel="noreferrer">ownly.io/stake</a> to earn a Ruler</p>
+                            <p className="mq-table-types-sub gotham-light font-size-120 text-white text-center">Stake 15M OWN for 60 Days at <a href="https://ownly.io/stake" className="link-color-2" target="_blank" rel="noreferrer">ownly.io/stake</a> to earn a Marauder</p>
                         </div>
                     </div>
                     <div className="col-12 col-md-4">
                         <div className="mq-table-types">
                             <div className="mq-table-types-img">
-                                <img className="w-100" src={redeemers} alt="Mustachio Redeemers" />
+                                <img className="w-100" src={rascals} alt="Mustachio Rascals" />
                             </div>
 
                             <p className="mq-table-types-sub gotham-semibold font-size-130 text-white text-center mb-2">10,000 MUSTACHIOS</p>
@@ -61,104 +61,116 @@ export default function Table(props) {
                             <thead>
                                 <tr className="font-size-70 font-size-md-100">
                                     <th className="text-center align-middle gotham-bold text-white font-size-150">Utilities and Perks</th>
-                                    <th className="text-center align-middle gotham-bold text-white font-size-150">Mustachio Royals</th>
-                                    <th className="text-center align-middle gotham-bold text-white font-size-150">Mustachio Rulers</th>
-                                    <th className="text-center align-middle gotham-bold text-white font-size-150">Mustachio Redeemers</th>
+                                    <th className="text-center align-middle gotham-bold text-white font-size-150">Mustachio Pathfinders</th>
+                                    <th className="text-center align-middle gotham-bold text-white font-size-150">Mustachio Marauders</th>
+                                    <th className="text-center align-middle gotham-bold text-white font-size-150">Mustachio Rascals</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td className="align-middle gotham-light text-white font-size-120">3D Playable Character</td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120">
+                                    <td className="align-middle gotham-light text-white font-size-100">3D Playable Character</td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100">
                                         <img src={greenCheck} alt="check" className="mq-table-check" />
                                     </td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120">
+                                    <td className="text-center align-middle gotham-light text-white font-size-100">
                                         <img src={orangeCheck} alt="check" className="mq-table-check" />
                                     </td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120">
+                                    <td className="text-center align-middle gotham-light text-white font-size-100">
                                         <img src={goldCheck} alt="check" className="mq-table-check" />
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td className="align-middle gotham-light text-white font-size-120">Exclusive Discord Channel</td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120">
+                                    <td className="align-middle gotham-light text-white font-size-100">Exclusive Discord Channel</td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100">
                                         <img src={greenCheck} alt="check" className="mq-table-check" />
                                     </td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120">
+                                    <td className="text-center align-middle gotham-light text-white font-size-100">
                                         <img src={orangeCheck} alt="check" className="mq-table-check" />
                                     </td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120">
+                                    <td className="text-center align-middle gotham-light text-white font-size-100">
                                         <img src={goldCheck} alt="check" className="mq-table-check" />
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td className="align-middle gotham-light text-white font-size-120">Intricately Hand-drawn Avatar by Boii Mustache</td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120">
+                                    <td className="align-middle gotham-light text-white font-size-100">Intricately Hand-drawn 2D PFP by Boii Mustache</td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100">
                                         <img src={greenCheck} alt="check" className="mq-table-check" />
                                     </td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120">
+                                    <td className="text-center align-middle gotham-light text-white font-size-100">
                                         <img src={orangeCheck} alt="check" className="mq-table-check" />
                                     </td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120"></td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100"></td>
                                 </tr>
                                 <tr>
-                                    <td className="align-middle gotham-light text-white font-size-120">Mirror on BNB Chain of 2D Hand-drawn PFP</td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120">
+                                    <td className="align-middle gotham-light text-white font-size-100">Mirror on BNB Chain of 2D Hand-drawn PFP</td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100">
                                         <img src={greenCheck} alt="check" className="mq-table-check" />
                                     </td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120">
+                                    <td className="text-center align-middle gotham-light text-white font-size-100">
                                         <img src={orangeCheck} alt="check" className="mq-table-check" />
                                     </td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120"></td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100"></td>
                                 </tr>
                                 <tr>
-                                    <td className="align-middle gotham-light text-white font-size-120">MustachioVerse Land Sale Discount</td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120">
+                                    <td className="align-middle gotham-light text-white font-size-100">MustachioVerse Land Sale Discount</td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100">
                                         <img src={greenCheck} alt="check" className="mq-table-check" />
                                     </td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120">
+                                    <td className="text-center align-middle gotham-light text-white font-size-100">
                                         <img src={orangeCheck} alt="check" className="mq-table-check" />
                                     </td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120"></td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100"></td>
                                 </tr>
                                 <tr>
-                                    <td className="align-middle gotham-light text-white font-size-120">Exclusive Land Access within Mustachio Quest</td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120">
+                                    <td className="align-middle gotham-light text-white font-size-100">Exclusive Land Access within Mustachio Quest</td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100">
                                         <img src={greenCheck} alt="check" className="mq-table-check" />
                                     </td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120"></td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120"></td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100"></td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100"></td>
                                 </tr>
                                 <tr>
-                                    <td className="align-middle gotham-light text-white font-size-120">10% Cryptoback in OWN</td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120">
+                                    <td className="align-middle gotham-light text-white font-size-100">10% Cryptoback in OWN</td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100">
                                         <img src={greenCheck} alt="check" className="mq-table-check" />
                                     </td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120"></td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120"></td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100"></td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100"></td>
                                 </tr>
                                 <tr>
-                                    <td className="align-middle gotham-light text-white font-size-120">Quarterly OWN Airdrop</td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120">
+                                    <td className="align-middle gotham-light text-white font-size-100">Quarterly OWN Airdrop</td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100">
                                         <img src={greenCheck} alt="check" className="mq-table-check" />
                                     </td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120"></td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120"></td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100"></td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100"></td>
                                 </tr>
                                 <tr>
-                                    <td className="align-middle gotham-light text-white font-size-120">The First NFT Tales and Story Behind Each Mustachio</td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120">
+                                    <td className="align-middle gotham-light text-white font-size-100">The First NFT Tales Behind Each Mustachio</td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100">
                                         <img src={greenCheck} alt="check" className="mq-table-check" />
                                     </td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120"></td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120"></td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100"></td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100"></td>
                                 </tr>
                                 <tr>
-                                    <td className="align-middle gotham-light text-white font-size-120">Generative Art</td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120"></td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120"></td>
-                                    <td className="text-center align-middle gotham-light text-white font-size-120">
+                                    <td className="align-middle gotham-light text-white font-size-100">Generative 3D Art</td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100"></td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100"></td>
+                                    <td className="text-center align-middle gotham-light text-white font-size-100">
                                         <img src={goldCheck} alt="check" className="mq-table-check" />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="border-left-none"></td>
+                                    <td className="border-bottom text-center">
+                                        <button onClick={props.mintBtn} className="btn link-color-2 p-0 font-size-100 font-size-sm-140">MINT NOW</button>
+                                    </td>
+                                    <td className="border-bottom text-center">
+                                        <a href="https://ownly.io/stake" className="link-color-2 p-0 font-size-100 font-size-sm-140" target="_blank" rel="noreferrer">STAKE OWN</a>
+                                    </td>
+                                    <td className="border-bottom align-middle text-center">
+                                        <p className="gotham-bold text-white font-size-80 font-size-sm-100 mb-0">LAUNCHING Q2 2022</p>
                                     </td>
                                 </tr>
                             </tbody>
