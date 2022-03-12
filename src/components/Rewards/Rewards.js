19e1e1c34562477bb52311c38486302f2d8d3a8f
Bernard Historillo <bernard.historillo@bicol-u.edu.ph>
2022-03-13 02:25:50 +0800

    Update image in rewards section to tiled images of marauders

--- src/components/Rewards/Rewards.js
+++ src/components/Rewards/Rewards.js
@@ -2,17 +2,17 @@ import './Rewards.css'
 
 // images
 import rewardsBG from '../../images/rewards-bg.jpg'
-import marauders from '../../images/marauders.png'
+import marauders from '../../images/marauders2.png'
 import bordertop from "../../images/sneakpeek-top.png";
 import borderbot from "../../images/sneakpeek-bot.png";
 
 export default function Rewards() {
     return (
         <section id="rewards" style={{"backgroundImage": `url(${rewardsBG})`}}>
-            <div className="container text-center">
-                <p className="rewards-title gotham-black text-white text-center font-size-170 mt-0 mt-xl-5 mb-5">EARN A MUSTACHIO MARAUDER NFT</p>
-                <div className="row align-items-center">
-                    <div className="col-12 col-md-6 order-2 order-md-1">
+            <div className="container text-center pb-5">
+                <p className="rewards-title gotham-black text-white text-center font-size-170 mt-0 mt-xl-5 mb-5">EARN A MUSTACHIO MARAUDER&nbsp;NFT</p>
+                <div className="row align-items-center pb-4">
+                    <div className="col-12 col-lg-5 order-2 order-lg-1">
                         <p className="text-white gotham-bold text-left font-size-100 font-size-md-130 font-size-lg-150 mb-3">HOW TO PARTICIPATE:</p>
                         <ul>
                             <li className="text-white">
@@ -38,13 +38,15 @@ export default function Rewards() {
                                 <p className="text-white text-left gotham-light font-size-90 font-size-md-110 font-size-lg-120 mb-2">MustachioVerse Land Sale Discount</p>
                             </li>
                         </ul>
-                        <div className="mt-5 mb-5  pb-5">
+                        <div className="mt-5">
                             <a href="https://ownly.io/ownmarauder" target="_blank" rel="noreferrer" className="btn btn-custom-2 gotham-black font-size-130 font-size-md-170 w-100">STAKE OWN NOW!</a>
                         </div>
                     </div>
-                    <div className="col-12 col-md-6 order-1 order-md-2 mb-5 mb-md-0">
-                        <div className="rewards-img">
-                            <img src={marauders} className="w-100" alt="Mustachio Marauders 2D" />
+                    <div className="col-12 col-lg-7 order-1 order-lg-2 mb-5 mb-lg-0 ps-lg-5">
+                        <div className="card" style={{'background-color':'#ffefcb'}}>
+                            <div className="card-body p-1">
+                                <img src={marauders} className="w-100" alt="Mustachio Marauders 2D" />
+                            </div>
                         </div>
                     </div>
                 </div>
