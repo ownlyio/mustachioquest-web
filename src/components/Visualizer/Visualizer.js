e478a69d242b954685abe9e3aa562a0d770860e4
Bernard Historillo <bernard.historillo@bicol-u.edu.ph>
2022-03-21 18:14:55 +0800

    Update visualizer section

--- src/components/Visualizer/Visualizer.js
+++ src/components/Visualizer/Visualizer.js
@@ -1,17 +1,19 @@
 import './Visualizer.css'
 
-import visualizerBG from '../../images/visualizer.jpg'
+import visualizerBG from '../../images/visualizer.png'
 import mqLogo from '../../images/visualizer-logo.png'
 
 export default function Visualizer() {
     return (
         <section id="visualizer" style={{"backgroundImage": `url(${visualizerBG})`}}>
             <div className="container h-100">
-                <div className="visualizer-wrap d-flex flex-column justify-content-center h-100">
-                    <div className="visualizer-img">
-                        <img src={mqLogo} alt="MQ Logo" className="w-100" />
+                <div className="visualizer-wrap d-flex flex-column align-items-center justify-content-center justify-content-lg-start align-items-lg-start h-100 pt-lg-5">
+                    <div className="text-center pt-lg-4">
+                        <div style={{"width": "280px"}}>
+                            <img src={mqLogo} alt="MQ Logo" className="w-100 mb-2" />
+                            <a href="https://my.mustachioverse.com" target="_blank" rel="noreferrer" className="btn btn-custom-2 gotham-black font-size-130 px-4 mx-3">LAUNCH IT NOW!</a>
+                        </div>
                     </div>
-                    <a href="https://my.mustachioverse.com" target="_blank" rel="noreferrer" className="visualizer-btn btn btn-custom-2 gotham-black font-size-140">LAUNCH IT NOW!</a>
                 </div>
             </div>
         </section>
