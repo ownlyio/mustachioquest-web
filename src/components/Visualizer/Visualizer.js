710fd8d2b81f809e88de7b046297419be2dee8fd
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-03-22 19:49:40 +0800

    Fix spelling

--- src/components/Visualizer/Visualizer.js
+++ src/components/Visualizer/Visualizer.js
@@ -12,7 +12,7 @@ export default function Visualizer() {
                         <img src={mqLogo} alt="MQ Logo" className="w-100 mb-2" />
                     </div>
                     <p className="visualizer-content text-white font-size-90 font-size-md-100 font-size-lg-110 font-size-xl-120 text-center">Introducing our Mustachio Visualizer. Test out your minted Mustachio from the Genesis Set, aka Mustachio Pathfinders, using your Metamask Wallet. Your Mustachio awaits you, wanderer!</p>
-                    <p className="visualizer-content text-white font-size-90 font-size-md-100 font-size-lg-110 font-size-xl-120 text-center">Other 3D Mustachio Pathfinders will be loaded soon.</p>
+                    <p className="visualizer-content text-white font-size-90 font-size-md-100 font-size-lg-110 font-size-xl-120 text-center">Other 3D Mustachio Pathfinders will be loaded soon. </p>
                     <a href="https://my.mustachioverse.com" target="_blank" rel="noreferrer" className="visualizer-btn btn btn-custom-2 gotham-black font-size-130 px-4 mx-3">LAUNCH IT NOW!</a>
                 </div>
             </div>
