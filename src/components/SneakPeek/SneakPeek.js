d56149266b6e7d82475dee1bf722ef6cf2b26d6d
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-03-23 14:22:07 +0800

    Fix Progress section items

--- src/components/SneakPeek/SneakPeek.js
+++ src/components/SneakPeek/SneakPeek.js
@@ -1,15 +1,15 @@
 import './SneakPeek.css'
 
 // images
-import sneak14 from '../../images/sneakpeeks/11-gif-slendie.jpg'
-import sneak13 from '../../images/sneakpeeks/10-gif-sludgie.jpg'
-import sneak12 from '../../images/sneakpeeks/9-squarehead.jpg'
-import sneak11 from '../../images/sneakpeeks/8-hotdog.jpg'
-import sneak10 from '../../images/sneakpeeks/7-loonywise.jpg'
-import sneak7 from '../../images/sneakpeeks/4-abracadoobie.jpg'
-import sneak6 from '../../images/sneakpeeks/3-mexico.png'
-import sneak5 from '../../images/sneakpeeks/2-desert-pit.png'
-import sneak3 from '../../images/sneakpeeks/sneakpeek-forge.png'
+import sneak9 from '../../images/sneakpeeks/9-gif-slendie.jpg'
+import sneak8 from '../../images/sneakpeeks/8-gif-sludgie.jpg'
+import sneak7 from '../../images/sneakpeeks/7-squarehead.jpg'
+import sneak6 from '../../images/sneakpeeks/6-hotdog.jpg'
+import sneak5 from '../../images/sneakpeeks/5-loonywise.jpg'
+import sneak4 from '../../images/sneakpeeks/4-abracadoobie.jpg'
+import sneak3 from '../../images/sneakpeeks/3-mexico.png'
+import sneak2 from '../../images/sneakpeeks/2-desert-pit.png'
+import sneak1 from '../../images/sneakpeeks/sneakpeek-forge.png'
 
 import bordertop from '../../images/sneakpeek-top.png'
 import borderbot from '../../images/sneakpeek-bot.png'
@@ -17,56 +17,56 @@ import borderbot from '../../images/sneakpeek-bot.png'
 export default function SneakPeek() {
     const sneak = [
         {
-            id: 14,
-            thumbnail: sneak14,
+            id: 9,
+            thumbnail: sneak9,
             link: "https://twitter.com/mustachioverse/status/1505148327440134150",
             title: "Wondering what Slendie is looking for this weekend?",
         },
         {
-            id: 13,
-            thumbnail: sneak13,
+            id: 8,
+            thumbnail: sneak8,
             link: "https://twitter.com/mustachioverse/status/1503384073959624710",
             title: "Just dropping a 360 degree tour with Sludgie.",
         },
         {
-            id: 12,
-            thumbnail: sneak12,
+            id: 7,
+            thumbnail: sneak7,
             link: "https://twitter.com/mustachioverse/status/1502973290066825216",
             title: "Say Hi to Squarehead!"
         },
         {
-            id: 11,
-            thumbnail: sneak11,
+            id: 6,
+            thumbnail: sneak6,
             link: "https://twitter.com/mustachioverse/status/1502528629305409536",
             title: "Hotdog Prince will teach you how to sprint and meet the other Mustachios!",
         },
         {
-            id: 10,
-            thumbnail: sneak10,
+            id: 5,
+            thumbnail: sneak5,
             link: "https://twitter.com/mustachioverse/status/1501070428122808320",
             title: "Here's Loonywise to say GM to y'all."
         },
         {
-            id: 7,
-            thumbnail: sneak7,
+            id: 4,
+            thumbnail: sneak4,
             link: "https://discord.com/channels/878131568806334464/935073739878563840/948221757607788594",
             title: "Abracadoobie Looking at the Horizon"
         },
         {
-            id: 6,
-            thumbnail: sneak6,
+            id: 3,
+            thumbnail: sneak3,
             link: "https://discord.com/channels/878131568806334464/935073739878563840/946207407271469076",
             title: "Mexico on the Desert"
         },
         {
-            id: 5,
-            thumbnail: sneak5,
+            id: 2,
+            thumbnail: sneak2,
             link: "https://discord.com/channels/878131568806334464/935073739878563840/945292032870580225",
             title: "Sneak Peek on the Desert Pit"
         },
         {
-            id: 3,
-            thumbnail: sneak3,
+            id: 1,
+            thumbnail: sneak1,
             link: "https://twitter.com/mustachioverse/status/1495748029005320198",
             title: "Enter the Forge!"
         },
