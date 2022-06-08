97e732dd1a5c93a8b185a641ba2cbf00fd073bec
Bernard Historillo <bernard.historillo@bicol-u.edu.ph>
2022-06-08 22:02:18 +0800

    Update banner

--- src/App.js
+++ src/App.js
@@ -287,8 +287,8 @@ export default function App() {
 
     return (
         <Router basename={process.env.PUBLIC_URL}>
-            <div className="position-fixed w-100" style={{"zIndex":"99999", "top":"0", "left":"0", "backgroundColor":"black"}}>
-                <div className="container text-center text-white font-size-80 py-1">Vigilantes’ Mint Date: Jun 9, 2022 - 8:00PM (SGT)</div>
+            <div className="position-fixed w-100" style={{"zIndex":"99999", "top":"0", "left":"0", "backgroundColor":"#ff9f42"}}>
+                <div className="container text-center text-black font-size-100 fw-bold py-2">Vigilantes’ Mint Date: Jun 9, 2022 - 8:00PM (SGT)</div>
             </div>
 
             <Navbar mintBtn={initUtilsAndMint} /> 
