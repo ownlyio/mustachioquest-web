5e2cedf32f0623d31f1694aee51383c75d99ef0c
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-07-26 11:31:00 +0800

    Update title of the Tales to a banner image

--- src/components/Tales/Tales.js
+++ src/components/Tales/Tales.js
@@ -2,6 +2,7 @@ import './Tales.css'
 
 import talesBanner from '../../images/tales-banner.jpg'
 import taleImg from '../../images/tale-img.png'
+import taleTitle from '../../images/tale-of-prospector.png'
 
 export default function Tales() {
     return (
@@ -10,7 +11,9 @@ export default function Tales() {
                 <div className="row h-100">
                     <div className="col-lg-7 col-12 h-100">
                         <div className="tales-wrap d-flex flex-column justify-content-center align-items-center h-100">
-                            <p className="tales-content text-white font-size-130 font-size-md-180 font-size-lg-220 font-size-xl-250 text-center">The Tale of the Prospector</p>
+                            <div className="tales-img-wrap">
+                                <img className="w-100" src={taleTitle} alt="Tale of the Prospector" />
+                            </div>
                             <p className="tales-content text-white font-size-100 font-size-md-100 font-size-lg-110 font-size-xl-120 text-center">O, when the Mustachios dwell in the MustachioVerse, there is but one Mustachio who stood out among the rest.</p>
                             <p className="tales-content text-white font-size-100 font-size-md-100 font-size-lg-110 font-size-xl-120 text-center">The Prospector, supreme beyond all MustachioKind, who bore in his mighty hands all 9 artifacts from the fabled Grooming Kit.</p>
                             <p className="tales-content text-white font-size-100 font-size-md-100 font-size-lg-110 font-size-xl-120 text-center">He who dared seek success and happiness through audacious exploits that moved mountains and changed the courses of the rivers.</p>
