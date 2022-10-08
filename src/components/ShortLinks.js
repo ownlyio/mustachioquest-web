13b716de7e94eeefed323e74f497471c35414377
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-10-08 09:45:03 +0800

    Add redirection of archetypes to homepage

--- src/components/ShortLinks.js
+++ src/components/ShortLinks.js
@@ -1,3 +1,12 @@
 export const Discord = () => {
     window.location.href = "http://discord.gg/mustachioverse";
 }
+export const Pathfinders = () => {
+    window.location.href = "https://mustachioverse.com";
+}
+export const Marauders = () => {
+    window.location.href = "https://mustachioverse.com";
+}
+export const Rascals = () => {
+    window.location.href = "https://mustachioverse.com";
+}
